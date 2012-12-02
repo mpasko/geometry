c2efc38ff00fc145a1aba618c882d950f2248da1
cudek <tomekcudek@gmail.com>
2012-12-02 09:58:07 +0100

    Dodanie liczenia aspect ratio. Signed-off-by:cudek <tomekcudek@gmail.com>

--- cpp/geometry/geometry.cpp
+++ cpp/geometry/geometry.cpp
@@ -10,6 +10,10 @@ double pitagoras(Point point1, Point point2) {
     return pitagoras(point1.x, point1.y, point2.x, point2.y);
 }
 
+double distance_from_segment(const Point& a, const Point&  b, const Point& p){
+    return fabs((b.x - a.x)*(a.y - p.y) -  (a.x - p.x)*(b.y - a.y))/pitagoras(a, b);
+}
+
 double orientation2d(Point pointA, Point pointB, Point pointC) {
     return (pointA.x - pointC.x) * (pointB.y - pointC.y) - (pointA.y - pointC.y) * (pointB.x - pointC.x);
 }
@@ -17,6 +21,26 @@ double orientation2d(Point pointA, Point pointB, Point pointC) {
 double orientation2D(Point pointA, Point pointB, Point pointC){
     return (pointA.x * pointB.y + pointA.y * pointC.x + pointB.x * pointC.y
             - pointA.x * pointC.y - pointA.y * pointB.x - pointB.y * pointC.x);
+}
+
+double get_aspect_ratio(const Point& a, const Point& b, const Point& c){
+    double dist_ab = pitagoras(a, b);
+    double dist_ac = pitagoras(a, c);
+    double dist_bc = pitagoras(b ,c);
+    
+    if (dist_ab > dist_ac){
+        if (dist_ab > dist_bc){
+            return dist_ab/distance_from_segment(a, b, c);
+        } else {
+            return dist_bc/distance_from_segment(b, c, a);
+        }
+    } else {
+        if (dist_ac > dist_bc){
+            return dist_ac/distance_from_segment(a, c, b);
+        } else {
+            return dist_bc/distance_from_segment(b, c, a);
+        }
+    }
 }
 
 bool are_points_equal(const Point& a, const Point& b){
